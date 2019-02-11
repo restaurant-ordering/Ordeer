36240e8e21b2801d877f474ef4bd61c5a04744ad
Braden Shipley <bradenrshipley@gmail.com>
2019-02-11 13:40:09 -0600

    testing firebase login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react'
-import firebase from '../firebase/firebase'
+import React from 'react'
+import firebase from './firebase/firebase'
+// import {auth, googleProvider} from './firebase/firebase'
 
 const App = props => {
 	return <div className='App'>Hello all</div>
