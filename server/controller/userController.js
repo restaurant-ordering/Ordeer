0d333d9321a13315ad458f24793823e7717306cb
Braden Shipley <bradenrshipley@gmail.com>
2019-02-11 16:59:10 -0600

    fix typo

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,6 +1,6 @@
-import { firebase, auth, googleProvider } from '../../src/firebase/firebase'
+import {firebase, auth, googleProvider} from '../../src/firebase/firebase'
 import firebase from 'firebase'
-const checkRestaurantEmail = (email) => {
+const checkRestaurantEmail = async email => {
 	let result = await firebase
 		.database()
 		.ref('restaurants')
@@ -16,11 +16,17 @@ const checkRestaurantEmail = (email) => {
 	}
 	return filterResult
 }
-const checkAdminEmail = (email) => {
-	let result = await firebase.database().ref('users').once('value').then(res => { return res.val() })
+const checkAdminEmail = async email => {
+	let result = await firebase
+		.database()
+		.ref('users')
+		.once('value')
+		.then(res => {
+			return res.val()
+		})
 	if (result) {
 		let filterResult
-		for (let i = 1; i < result.length; i++){
+		for (let i = 1; i < result.length; i++) {
 			if (result[i].email == email && result[i].isAdmin) {
 				filterResult = result[i]
 			}
@@ -34,36 +40,45 @@ const login = async (req, res, next) => {
 	if (result) {
 		//send to restaurant home page
 		try {
-			auth().signInWithPopup(googleProvider).then(result => {
-				let user = result.user
-				user.isRestaurant = true
-				res.status(200).send(user)
-			})
-		} catch{
+			auth()
+				.signInWithPopup(googleProvider)
+				.then(result => {
+					let user = result.user
+					user.isRestaurant = true
+					res.status(200).send(user)
+				})
+		} catch {
 			res.status(400).send('Could not login')
 		}
 	} else if (adminResult) {
 		try {
-			auth().signInWithPopup(googleProvider).then(result => {
-				let user = result.user
-				user.isAdmin = true
-				res.status(200).send(user)
-			})
-		} catch{
+			auth()
+				.signInWithPopup(googleProvider)
+				.then(result => {
+					let user = result.user
+					user.isAdmin = true
+					res.status(200).send(user)
+				})
+		} catch {
 			res.status(400).send('Could not login')
 		}
-	} else{
+	} else {
 		//send to user home page
-		auth().signInWithPopup(googleProvider).then(result => {
-			// let user = result.user
-			res.status(200).send(result.user)
-		})
+		auth()
+			.signInWithPopup(googleProvider)
+			.then(result => {
+				// let user = result.user
+				res.status(200).send(result.user)
+			})
 	}
 }
 const logout = (req, res, next) => {
-	auth().signOut().then(() => {
-		res.status(200).send('Logout Successful')
-	}).catch(err=>console.log(err))
+	auth()
+		.signOut()
+		.then(() => {
+			res.status(200).send('Logout Successful')
+		})
+		.catch(err => console.log(err))
 }
 const register = async (req, res, next) => {
 	const restaurantsRef = firebase.database().ref('restaurants')
