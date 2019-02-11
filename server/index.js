d66237be087175e8e86d25d3d4a365ce880c0b7b
Braden Shipley <bradenrshipley@gmail.com>
2019-02-11 11:44:10 -0600

    more boiler

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv') = config()
 const express = require('express')
 const { json } = require('bodyparser')
-const ct = require('./controllers/controller.js')
+
 const {SERVER_PORT} = process.env
 
 const app = express()
