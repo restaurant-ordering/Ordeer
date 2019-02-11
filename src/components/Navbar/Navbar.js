e1bf31cafad3233465d8945c098849c7d39685fe
Your Name <wamontgomery0068@gmail.com>
2019-02-11 13:33:04 -0600

    Imported Link and NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import './Navbar.css';
 
 const Navbar = props => {
 	return (
-		<>
-		</>
+		<div className = "Navbar_Container">
+			<Link to = "/home">
+			<div className = "Navbar_Login">
+				<p className = "Navbar_Login_Text"> Login </p>
+			</div>
+			</Link>
+		</div>
 	)
 }
 
