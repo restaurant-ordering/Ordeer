d8c249f159780a0d56a4c93f1ec5f40d42453afb
Your Name <wamontgomery0068@gmail.com>
2019-02-11 12:14:10 -0600

    Imported Navbar

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Landing.css';
 
 const Landing = props => {
 	return (
-		<>
-		</>
+		<div className = "Landing">
+			<Navbar />
+		</div>
 	)
 }
 
