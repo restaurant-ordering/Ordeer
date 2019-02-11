27bd5d8b5687811c9c787aa39f4b1e2f03338674
Your Name <wamontgomery0068@gmail.com>
2019-02-11 15:58:56 -0600

    Added CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import routes from "./routes";
 import firebase from './firebase/firebase';
+import './App.css';
 
 
 class App extends Component {
