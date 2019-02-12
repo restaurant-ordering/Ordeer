05b41e3dc3aefd20969a2ca1d9ceedcd7a51dc72
Your Name <wamontgomery0068@gmail.com>
2019-02-12 12:05:22 -0600

    Implemented Functionality. Need to test on Website. Need to style.

--- src/components/Forms/MenuForm/MenuDetails.js
+++ src/components/Forms/MenuForm/MenuDetails.js
@@ -1,139 +1,52 @@
 import React, { Component } from 'react';
-import MenuDetails from './MenuDetails';
-import FirstCategoryDetails from './FirstCategoryDetails';
-import SecondCategoryDetails from './SecondCategoryDetails';
-import ThirdCategoryDetails from './ThirdCategoryDetails';
-import FirstItemDetails from './FirstItemDetails';
-import SecondItemDetails from './SecondItemDetails';
-import ThirdItemDetails from './ThirdItemDetails';
-import MenuConfirmation from './MenuConfirmation';
-import MenuSuccess from './MenuSuccess';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
-export class MenuForm extends Component {
-  state = {
-    step: 1,
-    menu_name: '',
-    categories: '',
-    category_name: '',
-    menu_item: '',
-    image: '',
-    price: '',
-    description: '',
-    customization: ''
 
-  }
-
-  // Go to the Next Step Function
-  nextStep = () => {
-    const { step } = this.state;
-    this.setState({
-      step: step + 1
-    });
+export class MenuDetails extends Component {
+  continue = () => {
+    this.props.nextStep();
   };
 
-  // Handle the Input Field Change Function
-  handleChange = input => e => {
-    this.setState({ [input]: e.target.value });
-  };
-
-  // Go to the Previous Step Function
-  prevStep = () => {
-    const { step } = this.state;
-    this.setState({
-      step: step - 1
-    });
-  };
 
   render() {
-    const { step } = this.state;
-    const { menu_name, categories, menu_item, image, price, description, customization} = this.state;
-    const values = { menu_name, categories, menu_item, image, price, description, customization};
-
-    // Instead of a return statement we are going to use a switch statement
-    switch (step) {
-        case 1:
-        // Directs user to the MenuDetails Page
-        return (
-          <MenuDetails
-            nextStep={this.nextStep}
-            handleChange={this.handleChange}
-            values={values}
+    const { values, handleChange } = this.props;
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Enter Menu Details" />
+          <TextField
+            hintText="Enter Your Menu Name"
+            floatingLabelText="Menu Name"
+            onChange={handleChange('menu_name')}
+            defaultValue={values.menu_name}
           />
-        )
-        case 2:
-        // Directs user to the FirstCategoryDetails Page      
-        return (
-          <FirstCategoryDetails
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
+          <br />
+          <TextField
+            hintText="Enter the Number of Categories"
+            floatingLabelText="Category"
+            onChange={handleChange('categories')}
+            defaultValue={values.categories}
           />
-        );
-        case 3:
-        // Directs user to the FirstItemDetails Page
-        return (
-          <FirstItemDetails
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
+          <br />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
           />
-        );
-        case 4:
-        // Directs user to the SecondCategoryDetails Page
-        return (
-          <SecondCategoryDetails
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
-          />
-        );
-        case 5:
-        // Directs user to the SecondItemDetails Page
-        return (
-          <SecondItemDetails
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
-          />
-        );
-        case 6:
-        // Directs user to the ThirdCategoryDetails Page
-        return (
-          <ThirdCategoryDetails
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
-          />
-        );
-        case 7:
-        // Directs user to the ThirdItemDetails Page
-        return (
-          <ThirdItemDetails
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
-          />
-        );
-        case 8:
-        // Directs user to the MenuConfirmation Page
-        return (
-          <MenuConfirmation
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            values={values}
-          />
-        )
-        case 9:
-        // Directs user to the MenuSuccess Page
-        return <MenuSuccess />;
-    }
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
   }
 }
 
-export default MenuForm
+const styles = {
+  button: {
+    margin: 15
+  }
+};
+
+export default MenuDetails;
