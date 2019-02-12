3e0e6571fddd5b2c902999ff89987c61a1281355
Eric Willroth <ewillroth@gmail.com>
2019-02-12 14:25:38 -0600

    order page filters all restaurants and stores information regarding selected restaurant

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Order = props => {
+
+	const filterlocation = () => {
+		//gets the path from react router and returns the restaurant name
+		return props.location.pathname.split('/')[2]
+	}
+	//the name of the current restaurant
+	const restaurantname = filterlocation()
+	//stores all of the restaurants
+	const [restaurants, updateRestaurants] = useState([])
+	//gets all the restaurants from the backend
+	const getRestaurants = () => {
+		axios.get('/api/restaurants')
+			.then(response => {
+				//convert response to array of restaurant objects
+				let restaurantArray = []
+				for (let i in response.data) {
+					response.data[i].name = i
+					restaurantArray.push(response.data[i])
+				}
+				updateRestaurants(restaurantArray)
+			})
+			.catch(err => console.log(err))
+	}
+	//componentDidMount
+	useEffect(getRestaurants, [])
+	//gets current restaurant object from list of all restaurants
+	const restaurantObj = restaurants.filter(restaurant=>restaurant.name===restaurantname)[0]
+
+	console.log(restaurantObj)
+
 	return (
 		<>
 		</>
