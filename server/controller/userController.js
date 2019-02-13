134c9987470bbe597d8449acdd57e7f2e570a27f
Braden Shipley <bradenrshipley@gmail.com>
2019-02-13 16:09:34 -0600

    removed commented out code

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -17,61 +17,6 @@ const checkRestaurantEmail = async email => {
 		console.log('could not check if restaurant')
 	}
 }
-// const checkAdminEmail = async email => {
-// 	try {
-// 		let ref = await firebase.database().ref('users').once('value')
-// 		let result = await ref.val()
-// 		let filterResult
-// 		for (let i = 1; i < result.length; i++) {
-// 			if (result[i].email == email && result[i].isAdmin) {
-// 				filterResult = result[i]
-// 			}
-// 		}
-// 		return filterResult
-// 	} catch{
-// 		console.log('could not check if admin')
-// 	}
-// }
-// const login = async (req, res, next) => {
-// 	//i need to fix this one so please don't look at it too hard
-// 	let result = await checkRestaurantEmail(req.body.email)
-// 	let adminResult = await checkAdminEmail(req.body.email)
-// 	if (result) {
-// 		//send to restaurant home page
-// 		try {
-// 			let result = auth().signInWithPopup(googleProvider)
-// 			if (result) {
-// 				result => {
-// 					let user = result.user
-// 					user.isRestaurant = true
-// 					res.status(200).send(user)
-// 				}
-// 			}
-// 		} catch {
-// 			res.status(400).send('Could not login -restaurant')
-// 		}
-// 	} else if (adminResult) {
-// 		try {
-// 			let result = auth().signInWithPopup(googleProvider)
-// 			if (result) {
-// 				result => {
-// 					let user = result.user
-// 					user.isAdmin = true
-// 					res.status(200).send(user)
-// 				}
-// 			}
-// 		} catch {
-// 			res.status(400).send('Could not login -admin')
-// 		}
-// 	} else {
-// 		try {
-// 			auth().signInWithPopup(googleProvider)
-// 			res.sendStatus(200)
-// 		} catch {
-// 			res.status(400).send("Could not login as user")
-// 		}
-// 	}
-// }
 const logout = async (req, res, next) => {
 	auth().signOut()
 }
