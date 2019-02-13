85043634e284863783bb1b85d38f245e2a1c9c70
Eric Willroth <ewillroth@gmail.com>
2019-02-13 12:10:33 -0600

    rrestaurant form links to menu form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,9 +5,11 @@ import CreateMenu from './CreateMenu/CreateMenu';
 const Register = props => {
 
 	const [restaurant, updateRestaurant] = useState({})
+	
+	console.log(restaurant)
 
 	return (
-		!restaurant.email
+		Object.keys(restaurant).length===0
 		?
 		<CreateRestaurant updateRestaurant={updateRestaurant}/>
 		: 
