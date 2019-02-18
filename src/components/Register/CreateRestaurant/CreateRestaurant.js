83b3a8b91273065be3286d2b97abd9c68757c58b
Eric Willroth <ewillroth@gmail.com>
2019-02-18 14:32:43 -0600

    fix error that was icontinuously updating state

--- src/components/Register/CreateRestaurant/CreateRestaurant.js
+++ src/components/Register/CreateRestaurant/CreateRestaurant.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
