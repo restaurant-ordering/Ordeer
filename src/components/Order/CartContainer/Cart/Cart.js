0139420ec702024ab5603f735e9727772cdc3d16
Eric Willroth <ewillroth@gmail.com>
2019-02-20 13:35:51 -0600

    custom user on guest order says displayName rin place of name

--- src/components/Order/CartContainer/Cart/Cart.js
+++ src/components/Order/CartContainer/Cart/Cart.js
@@ -25,7 +25,7 @@ const Cart = props => {
 	const [flipped, flip] = useState(false)
 	const [redirect, activateRedirect] = useState(false)
 	const [values, setValues] = useState({
-		name: '',
+		displayName: '',
 		email: '',
 		phone: ''
 	})
@@ -101,7 +101,7 @@ const Cart = props => {
 				<div className="Cart">
 					<h1>Cart</h1>
 					<Card className={classes.card}>
-						<TextField onChange={onChange} name="name" placeholder="name"/>
+						<TextField onChange={onChange} name="displayName" placeholder="name"/>
 						<TextField onChange={onChange} name="email" placeholder="email"/>
 						<TextField onChange={onChange} name="phone" placeholder="phone number"/>
 						<CardActions>
