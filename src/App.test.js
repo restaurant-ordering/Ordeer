b56361aa8e2f5bfe351ee5179b8ba621b0310e20
Eric Willroth <ewillroth@gmail.com>
2019-02-21 10:56:06 -0600

    write 5 tests, refactor functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {createData} from './components/Admin/RestaurantTable/'
+import { firebase, auth, googleProvider } from './firebase/firebase'
+import { login, logout, checkAdminEmail, checkRestaurantEmail } from './functions/functions'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('createData', () => {
+	it('return an object with an id property that is one higher than the id in global scope', () => {
+		let id = 0
+		let expectedResult = 1
+		let actualResult = createData('name', 'address', 'city', 'state', 'zip', 'email')
+		expect(actualResult.id).toBe(expectedResult)
+	})
+})
+
+describe('checkRestaurantEmail', ()=>{
+	it('returns true for a valid restaurant email', async ()=>{
+		let email = 'kayceeshipley@gmail.com'
+		expect(await checkRestaurantEmail(email)).toBeTruthy()
+	})
+	it('returns false for an invalid restaurant email', async ()=>{
+		let email = "ordeerapp@gmail.com"
+		let actualResult = await checkRestaurantEmail(email)
+		expect(actualResult).toBeFalsy()
+	})
+})
+
+describe('checkAdminEmail', ()=>{
+	it('returns true for an valid Admin email', async ()=>{
+		let email = "ordeerapp@gmail.com"
+		let actualResult = await checkAdminEmail(email)
+		expect(actualResult).toBeTruthy()
+	})
+	it('returns false for an invalid Admin email', async ()=>{
+		let email = 'kayceeshipley@gmail.com'
+		let actualResult = await checkAdminEmail(email)
+		expect(actualResult).toBe(false)
+	})
+})
